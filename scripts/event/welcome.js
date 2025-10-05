a8cd2dad13c9399fcc7658c3ab3867dd1275d128
MC118CN <452044217@qq.com>
2025-10-05 12:43:23 +0800

    更新 v1.0.3

--- scripts/event/welcome.js
+++ scripts/event/welcome.js
@@ -1,15 +1,23 @@
 hexo.on('ready', () => {
     const { version } = require('../../package.json')
+    hexo.extend.injector.register('head_begin', `
+    <script>
+      window.MUHUTHEME_CONFIG = {
+        version: "${version}"
+      };
+    </script>
+    `, 'default');
+
     const logo = `
-    o     o o    oo o     o o    oo ####### #     # ####### #     # #######
-    oo   oo o    oo ooooooo o    oo    #    #     # #       ##   ## #     
-    o o o o o    oo o     o o    oo    #    ####### ####### # # # # #######
-    o  o  o  ooooo  o     o  ooooo     #    #     # #       #  #  # #
-                                       #    #     # ####### #     # #######
+    o     o o    oo o     o o    oo \x1b[38;5;45m####### #     # ####### #     # #######\x1b[0m
+    oo   oo o    oo ooooooo o    oo \x1b[38;5;45m   #    #     # #       ##   ## #     \x1b[0m
+    o o o o o    oo o     o o    oo \x1b[38;5;45m   #    ####### ####### # # # # #######\x1b[0m
+    o  o  o  ooooo  o     o  ooooo  \x1b[38;5;45m   #    #     # #       #  #  # #     \x1b[0m
+                                    \x1b[38;5;45m   #    #     # ####### #     # #######\x1b[0m
     `.replace(/#/g, '▉').replace(/o/g, '◼')
     const message = `
   \x1b[38;5;45m========================[ Hexo Theme - MuhuTheme ]=========================\x1b[0m
-  \x1b[38;5;45m${logo}\x1b[0m
+  ${logo}
                                \x1b[38;5;45m Version: ${version}\x1b[0m
   \x1b[38;5;45m===========================================================================\x1b[0m
                   \x1b[38;5;45mGitHub: https://github.com/Muhu-C/MuhuTheme\x1b[0m
